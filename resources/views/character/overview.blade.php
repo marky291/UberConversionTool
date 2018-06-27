1fdf3155132f25dda566b22afc49ef26b0ef36ee
Mark Hester <marky360@live.ie>
2018-06-27 01:03:09 +0100

    Updated to v1.1

--- resources/views/character/overview.blade.php
+++ resources/views/character/overview.blade.php
@@ -3,7 +3,7 @@
 @section('layout')
 
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow mb-4">
-        <h5 class="my-0 mr-md-auto font-weight-normal">XileRO Conversion Tool V1.0</h5>
+        <h5 class="my-0 mr-md-auto font-weight-normal">XileRO Conversion Tool V1.1</h5>
         <nav class="my-2 my-md-0 mr-md-3">
             <form action="{{ route('logout') }}" method="POST">
                 @csrf
